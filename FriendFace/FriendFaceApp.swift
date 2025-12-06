64b31c3f56c17d39011dc3a7c12a3193a755ebac
asi-isa <ali.inceoglu@protonmail.com>
2025-12-06 22:00:17 +0100

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Ali Soner Inceoglu on 06.12.25.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
